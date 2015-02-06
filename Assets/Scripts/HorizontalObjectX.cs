a66446074de3a71f05d277c5abe35543159625fc
Leedle42 <heerleigh42@gmail.com>
2015-02-06 10:01:59 -0800

    Trying to do player attatched to platform
    screw spelling

---
+++ Assets/Scripts/HorizontalObjectX.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorizontalObjectX : MonoBehaviour {
+	public float horizontalVelocity = 0.2f;
+	float timePassed = 0;
+	public float direction = 1;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (timePassed <= 3) {
+			transform.Translate(Vector3.left * horizontalVelocity * Time.deltaTime * direction, Space.World);
+		}
+		
+		if (timePassed >= 5) {
+			direction = direction * -1;
+			timePassed = 0;
+		}
+		timePassed += 1.0f * Time.deltaTime;
+
+	}
+}
