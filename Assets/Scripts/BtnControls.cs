d9636cecc0b55ec3555d31858958f6bdbac953ba
Dread134 <dread134@gmail.com>
2015-05-27 09:28:10 -0700

    Sounds fully implemented
    added click sounds and checkpoint sounds

--- Assets/Scripts/BtnControls.cs
+++ Assets/Scripts/BtnControls.cs
@@ -7,14 +7,18 @@ public class BtnControls : MonoBehaviour {
 
 	public GameObject exitMenu, optionsMenu, playBtn, optionsBtn, exitBtn, pauseScreen, Death_Overlay, DeathMenu,
 	Death_Text, Level_Text, SprintImage;
+	public AudioClip click;
+	private AudioSource clickSound;
 
 	void Awake () {
+		clickSound = this.GetComponent<AudioSource> ();
 		exitMenu.SetActive (false);
 		optionsMenu.SetActive (false);
 		pauseScreen.SetActive (false);
 	}
 
 	public void StartGame(){
+		clickSound.PlayOneShot(click);
 		Application.LoadLevel(1);
 		GameVariables.sprintSpeed = 1;
 		GameVariables.deaths = 0;
@@ -24,28 +28,33 @@ public class BtnControls : MonoBehaviour {
 	}
 	
 	public void ExitMenuShow(){
+		clickSound.PlayOneShot(click);
 		exitMenu.SetActive (true);
 		playBtn.SetActive (false);
 		optionsBtn.SetActive (false);
 		exitBtn.SetActive (false);
 	}
 	public void ExitMenuHide(){
+		clickSound.PlayOneShot(click);
 		exitMenu.SetActive (false);
 		playBtn.SetActive (true);
 		optionsBtn.SetActive (true);
 		exitBtn.SetActive (true);
 	}
 	public void ExitGame(){
+		clickSound.PlayOneShot(click);
 		Application.Quit ();
 	}
 
 	public void OptMenuShow(){
+		clickSound.PlayOneShot(click);
 		optionsMenu.SetActive (true);
 		playBtn.SetActive (false);
 		optionsBtn.SetActive (false);
 		exitBtn.SetActive (false);
 	}
 	public void OptMenuHide(){
+		clickSound.PlayOneShot(click);
 		optionsMenu.SetActive (false);
 		playBtn.SetActive (true);
 		optionsBtn.SetActive (true);
@@ -53,14 +62,17 @@ public class BtnControls : MonoBehaviour {
 	}
 	
 	public void ResumeGame(){
+		clickSound.PlayOneShot(click);
 		GameVariables.paused = false;
 		GameVariables.resume = true;
 		pauseScreen.SetActive (false);
 	}
 	public void ExitToMainMenu(){
+		clickSound.PlayOneShot(click);
 		Application.LoadLevel(0);
 	}
 	public void Respawn(){
+		clickSound.PlayOneShot(click);
 		GameVariables.respawn = true;
 		GameVariables.died = false;
 		DeathMenu.SetActive (false);
--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -5,13 +5,18 @@ public class Checkpoint : MonoBehaviour {
 	// initialize a variable that can be used in the unity inspector
 	// to simply change the direction the player respawns at each checkpoint
 	public float respawnDirection = 0;
+	public AudioClip checkpoint;
+	private AudioSource checkpointSound;
 
+	void Start () {
+		checkpointSound = this.GetComponent<AudioSource> ();
+	}
 	void OnTriggerEnter(Collider collider){
 		if (collider.gameObject.name == "Player") {
 			// set the new respawn position and rotation
 			GameVariables.checkpoint=transform.position;
 			GameVariables.checkpointRot = Quaternion.Euler(0, respawnDirection, 0);
-
+			checkpointSound.PlayOneShot(checkpoint);
 		}
 	}
 }
